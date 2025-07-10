9edd3b3f649699b8399aa1383b0b473b7e15b031
DYPatiluniversitytalsande <satyajitshinde498@gmail.com>
2025-07-10 11:56:39 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+
+//import Parent from "./Parent"
+//import Weather from "./Weather"
+import Netflix from "./Netflix"
+function App(){
+  return (
+      <div style={{"text-align":"center","border":"5px","color":"black",
+                     "background-color":"beige","font-family":"calibri"}}>  
+        <Netflix/>
+        {/* <Weather/> */}
+       </div> 
+  )
+}
+export default App;
